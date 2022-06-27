e3070dab11454a6b533393a9cc4840b1346f4ca3
David Carmichael <carmichaelits@gmail.com>
2022-06-27 11:58:29 +0100

    Modified run.py to show instructions to run with gunicorn.

--- run.py
+++ run.py
@@ -4,3 +4,11 @@ if __name__ == '__main__':
     create_app().run()
 else:
     gunicorn = create_app()
+
+"""
+gunicorn run:
+
+pip install gunicorn
+
+gunicorn -b 0.0.0.0:5000 -w 3 run:gunicorn
+"""
